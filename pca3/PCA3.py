28efd7feb0077e84da5ea71ffea1f01f1495cb53
rauldoe <khoado@csu.fullerton.edu>
2019-12-12 20:44:17 -0800

    pca3

---
+++ pca3/PCA3.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 18:25:34 2019
+
+@author: shlakhanpal
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io #Used to load *.mat files
+
+datafile = 'PCAData.mat'
+points = scipy.io.loadmat( datafile )
+
+X = points['X'] 
+# print('Printing data in X...')
+# print(X)
+m, n = X.shape
+
+#print(m, n)
+
+
+meanX = np.mean(X,axis =0)
+stdX = np.std(X,axis =0)
+
+normalized = (X - meanX)/stdX
+
+# print(meanX)
+# print(stdX)
+
+SumX = np.sum(X, axis =0)
+# print(SumX/50)
+
+print(normalized)
+covariance = np.matmul(normalized.transpose(), normalized)/(m-1)
+
+print(covariance)

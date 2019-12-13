6875ebbbbeed542d67d8dee25e7f561831eddc83
rauldoe <khoado@csu.fullerton.edu>
2019-12-12 22:09:41 -0800

    pca3 calc pca with graph and vector

--- class_exercise/pca/pca.py
+++ class_exercise/pca/pca.py
@@ -2,8 +2,27 @@
 import numpy as numpy
 import matplotlib.pyplot as plt
 import scipy.io
+from scipy.linalg import svd
 
 datafile = 'PCAData.mat'
 
 points = scipy.io.loadmat(datafile)
 
+A = points['X']
+print(points['X'])
+
+U, s, VT = svd(A)
+
+print(U)
+print(s)
+print(VT)
+
+x = [1,4,5,6,6,2,3]
+mean = sum(x)/len(x)
+std_dev = (1/len(x) * sum([ (x_i - mean)**2 for x_i in x]))**0.5
+
+z_scores = [(x_i - mean)/std_dev for x_i in x]
+
+# Min-Max scaling
+
+minmax = [(x_i - min(x)) / (max(x) - min(x)) for x_i in x]
